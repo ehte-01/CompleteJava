8f789c5b1bb8501f0204629966034971cadda973
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-01-23 00:54:54 +0530

    ....

---
+++ src/Multithreading/ThreadFunction/LambdaExpression.java
@@ -0,0 +1,12 @@
+package Multithreading.ThreadFunction;
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        Thread t1= new Thread( () -> {
+            for(int i =0; i<10; i++){
+                System.out.println("Hello World");
+            }
+        });
+        t1.start();
+    }
+}
