743c5950a6fc3215ea8c0592b1e2eefd8ae53103
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-01-31 12:37:00 +0530

    ..

--- src/CollectionFramework/Stack/StackDemo.java
+++ src/CollectionFramework/Stack/StackDemo.java
@@ -1,5 +1,7 @@
 package CollectionFramework.Stack;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class StackDemo {
@@ -22,7 +24,27 @@ public class StackDemo {
         System.out.println(stack.size());
 
         int search = stack.search(3);
-        
+        System.out.println(search);
+
+        //LinkedList as stack
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.addLast(1);
+        linkedList.addLast(2);
+        linkedList.addLast(3);
+        linkedList.getLast();
+        linkedList.removeLast();
+        linkedList.size();
+        linkedList.isEmpty();
+
+        // arrayList as stack
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.get(arrayList.size()-1);
+        arrayList.remove(arrayList.size() - 1);
 
     }
 }
+
