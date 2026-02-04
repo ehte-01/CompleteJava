42cb9f4da836fa0b3e1619848d641619509d7e0a
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-02-04 23:35:29 +0530

    ..

---
+++ src/CollectionFramework/Queue/PriorityQueueDemo.java
@@ -0,0 +1,27 @@
+package CollectionFramework.Queue;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+    public static void main(String[] args) {
+        // part of the queue interface
+        // orders elements based on their natural ordering ( for primitives lowest first)
+        // custom comparator for customized ordering
+        // does not allow null elements
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(15);
+        pq.add(10);
+        pq.add(30);
+        pq.add(5);
+        System.out.println(pq);
+        while (!pq.isEmpty()){
+            System.out.println(pq.poll());
+        }
+        System.out.println(pq.peek());
+        System.out.println(pq.remove());
+
+        // internal working
+        // PriorityQueue is impemented as a min-heap by default (for natural ordering)
+
+    }
+}
